5537242cb9d92fc8db5339b50da7cde32da81060
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 20:14:39 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -11,7 +11,7 @@ export default class DigitalClicker extends React.Component {
   handleClick = () => {
     this.setState(prevState => ({
       timesClicked += 1;
-    })
+    }))
   }
 
   render() {
