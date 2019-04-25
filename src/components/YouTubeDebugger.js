c08d4fcda326ee192620406b5dd70222f05b1e58
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 20:08:45 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,7 +3,14 @@ import React from 'react';
 export default class YouTubeDebugger extends React.Component {
   constructor() {
     this.state = {
-      
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
     }
   }
 }
